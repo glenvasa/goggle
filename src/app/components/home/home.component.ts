f1494b3f2156e6c7f9bcddf10fd2b6ca2762047c
glenvasa <70590043+glenvasa@users.noreply.github.com>
2021-02-17 15:26:46 -0500

    Update home.component.ts

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  search(form:NgForm): void{
+    // search_term is input from search.component.html
+    const{search_term} = form.value;
+    this.router.navigateByUrl('/results',{state: {term: search_term}}).then();
+    
   }
 
 }
