b5648e89636254b0ff0d3abb3ab005aeac1835b3
glenvasa <70590043+glenvasa@users.noreply.github.com>
2021-02-17 15:27:00 -0500

    Update search.component.ts

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,9 +1,16 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ControlContainer, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.scss']
+  styleUrls: ['./search.component.scss'],
+  // allows us to send search input value to parent component
+  viewProviders: [
+    {
+      provide: ControlContainer, useExisting: NgForm
+    }
+  ]
 })
 export class SearchComponent implements OnInit {
 @Input() currentTermValue: any;
