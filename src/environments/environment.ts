3ffd87112468a58f2e368feef9cd568d24463c5c
glenvasa <70590043+glenvasa@users.noreply.github.com>
2021-02-17 14:07:36 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  SERVER_URL: "https://www.googleapis.com/customsearch/v1",
+  API_KEY: "AIzaSyBUVgMqKmFyWMGOmv9JdU-ADh562SnxXNA",
+  CX_KEY: "308824764a7682b63",
+  // "017576662512468239146",
 };
 
 /*
