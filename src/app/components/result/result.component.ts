5937046a2f77fddb721953fa93e271dfa4adcf85
glenvasa <70590043+glenvasa@users.noreply.github.com>
2021-02-17 15:26:54 -0500

    Update result.component.ts

--- src/app/components/result/result.component.ts
+++ src/app/components/result/result.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { SearchService } from './../../search.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { GoogleResponse } from 'src/app/GoogleResponse.model';
 
 @Component({
   selector: 'app-result',
   templateUrl: './result.component.html',
   styleUrls: ['./result.component.scss']
 })
-export class ResultComponent implements OnInit {
+export class ResultComponent implements OnInit, OnDestroy {
+  results: GoogleResponse;
+  subs: Subscription[] =[];
+  term;
 
-  constructor() { }
 
+  constructor(private searchService: SearchService) { }
+  
   ngOnInit(): void {
+    const{term} = history.state;
+    this.term = term;
+
+    if(term){
+      this.subs.push(
+        this.searchService.getSearchData(term).subscribe((data: GoogleResponse)=>{
+          this.results = data;
+        })
+      );
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.subs.map(s => s.unsubscribe());
+  }
+
+  search(form:NgForm):void{
+    const{search_term} =form.value;
+    this.term =search_term;
+    this.subs.push(
+      this.searchService.getSearchData(search_term).subscribe((data: GoogleResponse)=>{
+        this.results = data;
+      })
+    );
   }
 
 }
