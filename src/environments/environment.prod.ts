a18306cd6452ba7e2d1e7428bf8cf79224dbe25a
glenvasa <70590043+glenvasa@users.noreply.github.com>
2021-02-17 16:08:46 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,4 @@ export const environment = {
   SERVER_URL: "https://www.googleapis.com/customsearch/v1",
   API_KEY: "AIzaSyBUVgMqKmFyWMGOmv9JdU-ADh562SnxXNA",
   CX_KEY: "7d50307aca40c7ebf:omuauf_lfve",
-  
 };
